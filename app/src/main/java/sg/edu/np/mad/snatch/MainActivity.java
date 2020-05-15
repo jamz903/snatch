e7f6baab65e3bd773fdaf67846d38224d18fffad
Jia He Justin Ng <s10195649@connect.np.edu.sg>
2020-05-15 16:56:22 +0800

    Created login page and initialised all the buttons and EditText views.

--- app/src/main/java/sg/edu/np/mad/snatch/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/snatch/MainActivity.java
@@ -4,11 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "snatch";
+    Button loginBtn;
+    Button signUpBtn;
+    EditText emailEditText;
+    EditText pwEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Created");
+
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        signUpBtn = (Button) findViewById(R.id.signUpBtn);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        pwEditText = (EditText) findViewById(R.id.pwEditText);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Started");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"Resumed");
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString();
+                String pw = pwEditText.getText().toString();
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
